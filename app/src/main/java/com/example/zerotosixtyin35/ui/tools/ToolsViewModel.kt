a694aca2ddc8e6ad558813fb5990e411a78a6aa6
Nimmi Penmatsa <nimmi@Nimmis-MacBook-Pro.local>
2019-08-25 18:38:15 -0400

    updated main activity and I now hate navigation

---
+++ app/src/main/java/com/example/zerotosixtyin35/ui/tools/ToolsViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.zerotosixtyin35.ui.tools
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class ToolsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is tools Fragment"
+    }
+    val text: LiveData<String> = _text
+}
